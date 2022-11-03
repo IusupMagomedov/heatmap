4772b4f05dadb7b79346b9328d0fa672d2d5f1c1
IusupMagomedov <yusub1988@gmail.com>
2022-11-03 10:47:22 +0300

    4 tests passed

--- src/App.js
+++ src/App.js
@@ -8,24 +8,56 @@ import * as d3 from "d3";
 function App() {
 
   const [dataSet, setDataSet] = useState([])
+  const [baseTemperature, setBaseTemperature] = useState(0)
   const svgRef = useRef(null)
   const w = 1603
   const h = 540
+  const padding = 60
 
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json")
       .then(response => response.json())
-      .then(data => setDataSet(data))
+      .then(data => {
+        setBaseTemperature(data.baseTemperature)
+        setDataSet(data.monthlyVariance)
+      })
       .catch(error => console.log(error))
-  
+    
     return () => {}
   }, [])
   
 
   useEffect(() => {
+    //console.log("Just fetched data in useEffect", dataSet)
+
     const svg = d3.select(svgRef.current)
       .attr("width", w)
       .attr("height", h)
+      .attr("id", "title")
+
+    const minYear = d3.min(dataSet, d => d.year)
+
+    const maxYear = d3.max(dataSet, d => d.year)
+
+    const cellWidth = (w - padding * 2) / (maxYear - minYear)
+    //console.log("Cell width: ", cellWidth)
+    const cellHeight = (h - padding * 2) / 12
+    //console.log("Cell height: ", cellHeight)
+    svg.selectAll("rect")
+      .data(dataSet)
+      .enter()
+      .append("rect")
+      .attr("width", cellWidth)
+      .attr("height", cellHeight)
+      .attr("x", d => padding + (d.year - minYear) * cellWidth)
+      .attr("y", d => padding + (d.month - 1) * cellHeight)
+      .attr("class", "cell")
+      .attr("data-month", d => d.month)
+      .attr("data-year", d => d.year)
+      .attr("data-temp", d => d.variance + baseTemperature)
+      .attr("fill", "blue")
+
+    
 
 
     return () => {} 
